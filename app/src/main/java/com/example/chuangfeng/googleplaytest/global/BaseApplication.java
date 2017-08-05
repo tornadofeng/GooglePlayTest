bd827259d117a73e5a58f9e98334f9f401451ed4
chuangfeng <947924200@qq.com>
2017-08-05 18:34:58 +0800

    First commit.

---
+++ app/src/main/java/com/example/chuangfeng/googleplaytest/global/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.example.chuangfeng.googleplaytest.global;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/8/5.
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+    }
+}
